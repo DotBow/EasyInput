69b99d475778000531aad91df925a2abbee0ae82
DotBow <valveoleg@gmail.com>
2025-10-15 14:57:28 +0300

    Add special component for easier binding

---
+++ Source/EasyInput/Private/EasyInputComponent.cpp
@@ -0,0 +1,61 @@
+﻿// Copyright (C) Dreamer's Tail
+
+#include "EasyInputComponent.h"
+
+#include "EasyInputBindings.h"
+#include "GameFramework/Character.h"
+
+
+UEasyInputComponent::UEasyInputComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+FGameplayTag UEasyInputComponent::GetActiveInputTag() const
+{
+	return ActiveInputTag;
+}
+
+void UEasyInputComponent::SetActiveInputTag(
+	const FGameplayTag& InActiveInputTag)
+{
+	if (!ensureMsgf(InputTags.HasTag(InActiveInputTag),
+		TEXT("Unknown input tag!")))
+		return;
+
+	ActiveInputTag = InActiveInputTag;
+}
+
+void UEasyInputComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ACharacter* Character = Cast<ACharacter>(GetOwner());
+
+	if (!ensureMsgf(Character,
+		TEXT("UEasyInputComponent requires ACharacter outer!")))
+		return;
+
+	APlayerController* PlayerController = Cast<APlayerController>(
+		Character->GetController());
+
+	if (!ensureMsgf(PlayerController,
+		TEXT("Player Controller is invalid!")))
+		return;
+
+	SetInputBindings(
+		PlayerController, InputBindings, Character);
+}
+
+void UEasyInputComponent::SetInputBindings(
+	const TObjectPtr<APlayerController>& PlayerController,
+	UEasyInputBindings* InInputBindings,
+	UObject* Object)
+{
+	if (!InputTags.HasTag(InInputBindings->InputTag))
+	{
+		UEasyInputBindings::SetupKeyBindings(
+			PlayerController, Object, InInputBindings);
+		InputTags.AddTag(InInputBindings->InputTag);
+	}
+}
---
+++ Source/EasyInput/Public/EasyInputComponent.h
@@ -0,0 +1,40 @@
+﻿// Copyright (C) Dreamer's Tail
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+#include "Components/ActorComponent.h"
+#include "EasyInputComponent.generated.h"
+
+
+UCLASS(Within=Character, ClassGroup=(Input), meta=(BlueprintSpawnableComponent))
+class EASYINPUT_API UEasyInputComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEasyInputComponent();
+
+	FGameplayTag GetActiveInputTag() const;
+
+	void SetActiveInputTag(
+		const FGameplayTag& InActiveInputTag);
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	TObjectPtr<class UEasyInputBindings> InputBindings;
+
+	UPROPERTY()
+	FGameplayTagContainer InputTags;
+
+	UPROPERTY(VisibleAnywhere)
+	FGameplayTag ActiveInputTag;
+
+protected:
+	virtual void BeginPlay() override;
+
+	void SetInputBindings(
+		const TObjectPtr<APlayerController>& PlayerController,
+		UEasyInputBindings* InInputBindings,
+		UObject* Object);
+};
